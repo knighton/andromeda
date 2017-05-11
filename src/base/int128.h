cc539a5622c85ed455727db34c719af36b47e4b9
James Knighton <iamknighton@gmail.com>
2017-05-11 09:54:23 -0700

    Prototype heatspace lookup functions.

---
+++ src/base/int128.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace andromeda {
+namespace base {
+
+// Requires -std=gnu++.
+typedef __int128 int128_t;
+typedef unsigned __int128 uint128_t;
+
+}  // namespace base
+}  // namespace andromeda
